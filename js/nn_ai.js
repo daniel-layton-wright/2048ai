4456b7dbd9ee4a1e70fd781de5d2c99a415351bc
daniel-layton-wright <daniel.layton.wright@gmail.com>
2024-03-28 13:19:58 -0700

    Fix so can't try illegal move

--- js/nn_ai.js
+++ js/nn_ai.js
@@ -14,27 +14,16 @@ NNAI.prototype.nextMove = async function() {
     output = await this.session.run([tensor]);
     var outputTensor = output.values().next().value;
     var outputArray = Array.from(outputTensor.data);
-    var maxIndex = outputArray.indexOf(Math.max(...outputArray));
-    var direction = maxIndex % 4;
 
-    // direction currently is  0:left 1:right 2:up 3:down
-    // convert to 0: up, 1:right, 2: down, 3: left
-    switch (direction) {
-        case 0:
-            direction = 3;
-            break;
-        case 1:
-            direction = 1;
-            break;
-        case 2:
-            direction = 0;
-            break;
-        case 3:
-            direction = 2;
-            break;
+    // Any move that is not available should be set to -Infinity
+    for (var i = 0; i < 4; i++) {
+        if (!this.game.moveAvailable(i)) {
+            outputArray[gameDirectionToNNDirection(i)] = -Infinity;
+        }
     }
 
-    return direction;
+    var maxIndex = outputArray.indexOf(Math.max(...outputArray));
+    return nnDirectionToGameDirection(maxIndex % 4);
 };
 
 gridToTensor = function(grid) {
@@ -60,4 +49,49 @@ gridToTensor = function(grid) {
 
     tensor = new onnx.Tensor(new Float32Array(gridArray), 'float32', [1, 16, 4, 4]);
     return tensor;
+};
+
+nnDirectionToGameDirection = function(nnDirection) {
+
+    var direction = 0;
+    // direction currently is  0:left 1:right 2:up 3:down
+    // convert to 0: up, 1:right, 2: down, 3: left
+    switch (nnDirection) {
+        case 0:
+            direction = 3;
+            break;
+        case 1:
+            direction = 1;
+            break;
+        case 2:
+            direction = 0;
+            break;
+        case 3:
+            direction = 2;
+            break;
+    }
+
+    return direction;
+};
+
+gameDirectionToNNDirection = function(gameDirection) {
+    var direction = 0;
+    // direction currently is 0: up, 1:right, 2: down, 3: left
+    // convert to 0:left 1:right 2:up 3:down
+    switch (gameDirection) {
+        case 0:
+            direction = 2;
+            break;
+        case 1:
+            direction = 1;
+            break;
+        case 2:
+            direction = 3;
+            break;
+        case 3:
+            direction = 0;
+            break;
+    }
+
+    return direction;
 };
